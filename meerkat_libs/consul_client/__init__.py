aa8932087937a903be374f5f2fca7ff17f0f520b
Tomasz Sabała <sabala.tomek@gmail.com>
2017-10-17 15:50:02 +0200

    Added event buffer flush.

--- meerkat_libs/consul_client/__init__.py
+++ meerkat_libs/consul_client/__init__.py
@@ -8,6 +8,7 @@ import requests
 CONSUL_URL = environ.get("CONSUL_URL", "http://nginx/consul")
 DHIS2_EXPORT_ENABLED = environ.get("DHIS2_EXPORT_ENABLED", False)
 
+
 @backoff.on_exception(backoff.expo, requests.exceptions.ConnectionError, max_tries=8, max_value=30)
 def initialize_dhis2():
     logging.info("Initializing consul publisher")
@@ -42,8 +43,20 @@ def send_dhis2_events(uuid=None, raw_row=None, form_id=None):
     )
     if len(events_buffer) > 500:
         logging.info("Sending batch of events to consul.")
-        json_payload = json.dumps(
-            {"Messages": events_buffer}
-        )
-        requests.post(CONSUL_URL + "/dhis2/export/events", json=json_payload)
-        events_buffer = []
+        __send_events_from_buffer()
+
+
+def flush_dhis2_events():
+    if not DHIS2_EXPORT_ENABLED:
+        return
+    logging.info("Clearing Consul Client event buffer.")
+    __send_events_from_buffer()
+
+
+def __send_events_from_buffer():
+    global events_buffer
+    json_payload = json.dumps(
+        {"Messages": events_buffer}
+    )
+    requests.post(CONSUL_URL + "/dhis2/export/events", json=json_payload)
+    events_buffer = []
