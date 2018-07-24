4b4419c132d9638a0ad14255c12e7f05412de317
Tomasz Sabała <sabala.tomek@gmail.com>
2018-07-24 14:15:13 +0200

    Introduced a single endpoint for all form submissions.

--- meerkat_libs/consul_client/__init__.py
+++ meerkat_libs/consul_client/__init__.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import jwt
+import collections
 from os import environ
 
 import backoff as backoff
@@ -22,7 +23,7 @@ def initialize_dhis2():
     logging.info("DONE: Initializing consul publisher")
 
 
-events_buffer = []
+events_buffer = collections.defaultdict(list)
 
 
 def send_dhis2_events(uuid=None, raw_row=None, form_id=None, auth_token=None):
@@ -38,7 +39,7 @@ def send_dhis2_events(uuid=None, raw_row=None, form_id=None, auth_token=None):
                       }
     # TODO: Should md5 be generated here?
     md5_of_body = ""
-    events_buffer.append(
+    events_buffer[form_id].append(
         {
             'MessageId': uuid,
             'ReceiptHandle': 'test-receipt-handle-1',
@@ -49,9 +50,9 @@ def send_dhis2_events(uuid=None, raw_row=None, form_id=None, auth_token=None):
             }
         }
     )
-    if len(events_buffer) > 500:
+    if len(events_buffer[form_id]) > 500:
         logging.info("Sending batch of events to consul.")
-        __send_events_from_buffer(auth_token)
+        __send_events_from_buffer(form_id=form_id, auth_token=auth_token)
 
 
 def flush_dhis2_events(auth_token=None):
@@ -60,17 +61,19 @@ def flush_dhis2_events(auth_token=None):
     if not auth_token:
         logging.error("No authentication token provided.")
         return
-    logging.info("Clearing Consul Client event buffer.")
-    __send_events_from_buffer(auth_token)
+    for form_id in events_buffer:
+        logging.info("Clearing Consul Client event buffer for %s.", form_id)
+        __send_events_from_buffer(form_id=form_id, auth_token=auth_token)
 
 
-def __send_events_from_buffer(auth_token):
+def __send_events_from_buffer(form_id=None, auth_token=None):
     global events_buffer
     json_payload = json.dumps(
-        {"Messages": events_buffer}
+        {"formId": form_id,
+         "Messages": events_buffer[form_id]}
     )
-    requests.post(CONSUL_URL + "/dhis2/export/events", headers=_auth_headers(auth_token), json=json_payload)
-    events_buffer = []
+    requests.post(CONSUL_URL + "/dhis2/export/submissions", headers=_auth_headers(auth_token), json=json_payload)
+    events_buffer[form_id] = []
 
 
 def _auth_headers(token):
